59afe448f99d6bcdb56d55ebfedc52baf0d47d86
Maxime JACOUPY <m.jacoupy@institut-myologie.org>
2024-04-15 17:19:43 +0200

    docs: full in english, including var names

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-# #######################################################################################################################
-#                                              # === LIBRAIRIES === #
-# #######################################################################################################################
+#####################################################################
+# =========================== LIBRAIRIES ========================== #
+#####################################################################
+
 import pandas as pd
 import numpy as np
 from flask import Flask, render_template, request, send_file
@@ -9,31 +10,31 @@ import io
 
 
 #####################################################################
-# ========================= INFO GENERALES========================= #
+# ========================= GENERAL INFO ========================== #
 #####################################################################
 
 app = Flask(__name__)
 
 
 #####################################################################
-# ==================== FONCTIONS D'ASSISTANCES ==================== #
+# ===================== ASSISTANCE FUNCTIONS ====================== #
 #####################################################################
 
 def convert_df(df):
     """
-    Convertit un DataFrame pandas en une chaîne d'octets, en utilisant ";" comme séparateur de colonnes,
-    et encode le résultat en latin1.
+    Converts a pandas DataFrame to a byte string, using ";" as the column separator,
+    and encodes the result in Latin1.
 
     Parameters:
-    - df (pandas.DataFrame): Le DataFrame à convertir.
+    - df (pandas.DataFrame): The DataFrame to convert.
 
     Returns:
-    - bytes: La chaîne d'octets résultante, encodée en latin1.
+    - bytes: The resulting byte string, encoded in Latin1.
     """
-    # Utilisation de ";" comme séparateur de colonnes personnalisé
+    # Use ";" as a custom column separator
     csv_data = df.to_csv(sep=';', index=False)
 
-    # Encodage des données CSV en utf-8 pour éviter les erreurs liées aux caractères
+    # Encoding the CSV data to Latin1 to ensure compatibility with systems that require this encoding
     encoded_data = csv_data.encode('latin1')
 
     return encoded_data
@@ -41,73 +42,73 @@ def convert_df(df):
 @app.route('/', methods=['GET'])
 def index():
     """
-    Route principale qui affiche la page d'accueil.
+    Main route that displays the homepage.
 
     Returns:
-    - str: Le contenu HTML de la page d'accueil.
+    - str: The HTML content of the homepage.
     """
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_files():
     """
-    Traite le téléchargement de fichiers CSV, les trie selon l'ordre spécifié par l'utilisateur,
-    fusionne les données et envoie le fichier CSV fusionné au client.
+    Processes the upload of CSV files, sorts them according to the user-specified order,
+    merges the data, and sends the merged CSV file back to the client.
 
     Returns:
-    - send_file: Un objet Flask `send_file` contenant le fichier CSV fusionné.
+    - send_file: A Flask `send_file` object containing the merged CSV file.
     """
     uploaded_files = request.files.getlist("csv_files[]")
-    file_order = request.form.getlist("file_order[]")  # Récupérer l'ordre des fichiers
+    file_order = request.form.getlist("file_order[]")  # Retrieve the file order
 
-    # Créer un dictionnaire pour mapper le nom du fichier à son ordre
+    # Create a dictionary to map file name to its order
     file_order_dict = {file.filename: int(order) for file, order in zip(uploaded_files, file_order)}
 
-    # Trier les fichiers téléchargés selon l'ordre spécifié
+    # Sort the uploaded files according to the specified order
     uploaded_files.sort(key=lambda file: file_order_dict.get(file.filename, 999))
 
-    # Suite du traitement
-    dfs = []
+    dfs = []  # List to hold dataframes for merging
 
-    # Boucle sur chaque fichier téléchargé dans la liste 'uploaded_files'
-    first_rows = []  # Ajoutez cette ligne
+    # Loop through each uploaded file in the 'uploaded_files' list
+    first_rows = []  # Add this line
     for iCpt, uploaded_file in enumerate(uploaded_files):
+        # Read the CSV file using pandas, specifying the ';' separator and 'latin1' encoding
         df = pd.read_csv(uploaded_file, sep=';', encoding='latin1', engine='python')
         first_rows.append(df.head())
 
-        # Vérifie si la colonne 'initiales_nom' existe dans le DataFrame
+        # Check if the 'initiales_nom' column exists in the DataFrame
         if 'initiales_nom' in df.columns:
-            # Si 'initiales_nom' existe, défini la colonne comme index et ajoute un préfixe à chaque nom de colonne
+            # If 'initiales_nom' exists, set the column as index and add a prefix to each column name
             df.set_index('initiales_nom')
             df = df.add_prefix(str(iCpt+1)+'_')
 
-        # Vérifie si la colonne 'numero_patient' existe dans le DataFrame
+        # Check if the 'numero_patient' column exists in the DataFrame
         elif 'numero_patient' in df.columns:
             df.rename(columns = {'numero_patient':'initiales_nom'}, inplace = True)
             df.set_index('initiales_nom')
             df = df.add_prefix(str(iCpt+1)+'_')
 
-        # Si la première colonne contient des nombres, appelle la colonne "initiales_nom" comme index et ajoute un préfixe à chaque nom de colonne
+        # If the first column contains numbers, set the column "initiales_nom" as index and add a prefix to each column name
         elif pd.Series(df.iloc[1, 0]).dtype in (int, float):
             df = df.rename(columns={df.columns[0]: 'initiales_nom'})
             df.set_index('initiales_nom')
             df = df.add_prefix(str(iCpt+1)+'_')
 
         else:
-            # Si 'initiales_nom' n'existe pas, combine les colonnes 'last_name' et 'first_name' pour créer la colonne 'initiales_nom'
+            # If 'initiales_nom' does not exist, combine the 'last_name' and 'first_name' columns to create the 'initiales_nom' column
             df.insert(0, 'initiales_nom', df['last_name'].astype(str) + df['first_name'].astype(str))
             df.set_index('initiales_nom')
             df = df.add_prefix(str(iCpt+1)+'_')
 
-        # # Affiche les 5 premières lignes du DataFrame
+        # Print the first 5 rows of the DataFrame
         print(f"File {iCpt+1}: {uploaded_file.filename}")
         print(df.head())
 
-        # Stocke le DataFrame dans une variable globale avec un nom unique et ajoute le DataFrame à la liste 'dfs'
+        # Store the DataFrame in a global variable with a unique name and append the DataFrame to the 'dfs' list
         globals()['df%s' % iCpt] = df
         dfs.append(globals()['df%s' % iCpt])
 
-    # Fusionner les dataframes de la liste l horizontalement (en colonnes)
+    # Merge the DataFrames from the list horizontally (in columns)
     concat_df = pd.concat(dfs, axis=1)
     concat_df.rename(columns={concat_df.columns[0]: 'initiales_nom'}, inplace=True)
 
@@ -132,11 +133,11 @@ def download_csv():
 
 
 #####################################################################
-# ====================== LANCEMENT DE L'ALGO ====================== #
+# ========================== ALGO LAUNCH ========================== #
 #####################################################################
 
 if __name__ == '__main__':
     """
-    Point d'entrée principal pour l'application Flask. Exécute l'application en mode debug.
+    Main entry point for the Flask application. Runs the application in debug mode.
     """
     app.run(debug=True)
